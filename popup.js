d98fac9177de9e69195523bc450da0928a0d3c4d
cnStevenYu <397729364@qq.com>
2016-03-31 09:44:21 +0800

    fix bug

--- popup.js
+++ popup.js
@@ -3,23 +3,23 @@ var queryBaiduUrl = "http://apis.baidu.com/apistore/stockservice/stock";
 
 //Baidu Stock Information Api Key
 //!!!!!Please visit site http://apistore.baidu.com/apiworks/servicedetail/115.html to apply for a api key
-var apiKey = '';
+var apiKey = '35cef313060e63e4fee417308aa83925';
 
 //Message
 var MSG = {
     CODE_SIZE_ERROR: '请输入6位股票代码!',
-    CODE_ERROR:'股票代码不正确!',
-    NETWORK_ERROR:'网络连接错误!',
-    SERVER_ERROR:'服务器错误!',
-    ADD_INFO:'添加成功!',
-    DEL_INFO:'删除成功!'
+    CODE_ERROR: '股票代码不正确!',
+    NETWORK_ERROR: '网络连接错误!',
+    SERVER_ERROR: '服务器错误!',
+    ADD_INFO: '添加成功!',
+    DEL_INFO: '删除成功!'
 };
 
 //DOM show message
 function showMsg(str) {
     var alter = '';
     $('#Alert').empty();
-    if(str === MSG.ADD_INFO || str === MSG.DEL_INFO) {
+    if (str === MSG.ADD_INFO || str === MSG.DEL_INFO) {
         alter =
             '<div class=\"alert alert-info\" style=\"padding: 5px; margin-bottom: 0px\">' +
             '<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\" style=\"font-size: inherit\">\&times;</a>' +
@@ -37,15 +37,15 @@ function showMsg(str) {
 }
 
 //DOM update stock information and color
-function changeStockInfoAndColor(stockInfo){
+function changeStockInfoAndColor(stockInfo) {
     var cls = 'info';
 
     var rate = stockInfo['increase'];
 
-    if(rate < 0) cls = 'success';//show green
-    else if(rate > 0) cls = 'danger';//show red
+    if (rate < 0) cls = 'success';//show green
+    else if (rate > 0) cls = 'danger';//show red
 
-    return "<tr " + "class=" + cls +  ">" +
+    return "<tr " + "class=" + cls + ">" +
         "<td>" +
         stockInfo['name'] +
         "</td>" +
@@ -64,11 +64,11 @@ function changeMarketInfoAndColor(marketInfo) {
 
     var rate = marketInfo['rate'];
 
-    if(rate<0) cls = 'label label-success';//show green
-    else if(rate>0) cls = 'label label-danger';//show red
+    if (rate < 0) cls = 'label label-success';//show green
+    else if (rate > 0) cls = 'label label-danger';//show red
 
 
-    switch (marketInfo['market']){
+    switch (marketInfo['market']) {
         case 'shanghai':
             $('#ShPrice').removeClass();
             $('#ShIns').removeClass();
@@ -107,7 +107,7 @@ function removeStockInfo() {
 }
 
 //query and set stock information
-function getStockInfo(){
+function getStockInfo() {
     chrome.storage.sync.get(null, function (items) {
 
         var stockIds = [];
@@ -116,11 +116,15 @@ function getStockInfo(){
             stockIds.push(items[key]);
         }
 
-        if (stockIds.length > 0) {
-            var stockIdPars = stockIds.join(',');
+        var stockIdPars = '';
 
-            queryStockInfo(stockIdPars, function(data){ //query success
+        if (stockIds.length === 0)
+            stockIdPars = '000001';//not be empty.
+        else
+            stockIdPars = stockIds.join(',');
 
+        queryStockInfo(stockIdPars,
+            function (data) { //query success
                 console.log(data);
                 if (data['errMsg'] != 'success') {
                     showMsg(MSG.SERVER_ERROR);
@@ -129,45 +133,52 @@ function getStockInfo(){
 
                 //set market
                 var marketInfo = data['retData']['market'];
-                changeMarketInfoAndColor({'market': 'shanghai',
+                changeMarketInfoAndColor({
+                    'market': 'shanghai',
                     'dot': marketInfo['shanghai']['curdot'].toFixed(0),
-                    'rate': marketInfo['shanghai']['rate']});
-                changeMarketInfoAndColor({'market': 'shenzhen',
+                    'rate': marketInfo['shanghai']['rate']
+                });
+                changeMarketInfoAndColor({
+                    'market': 'shenzhen',
                     'dot': marketInfo['shenzhen']['curdot'].toFixed(0),
-                    'rate': marketInfo['shenzhen']['rate']});
-                changeMarketInfoAndColor({'market': 'hsi',
+                    'rate': marketInfo['shenzhen']['rate']
+                });
+                changeMarketInfoAndColor({
+                    'market': 'hsi',
                     'dot': marketInfo['HSI']['curdot'].toFixed(0),
-                    'rate': marketInfo['HSI']['rate']});
+                    'rate': marketInfo['HSI']['rate']
+                });
 
-                //remove the existing stock
-                removeStockInfo();
+                if (stockIds.length > 0) {//if user has added stock ids.
 
-                //set every stock
-                var stockInfo = data['retData']['stockinfo'];
+                    //remove the existing stock
+                    removeStockInfo();
 
-                for (var i = 0; i < stockInfo.length; ++i) {
+                    //set every stock
+                    var stockInfo = data['retData']['stockinfo'];
 
-                    var stock = {
-                        'name': stockInfo[i]['name'],
-                        'price': stockInfo[i]['currentPrice'] === 0 ? stockInfo[i]['closingPrice'] : stockInfo[i]['currentPrice'],
-                        'increase': stockInfo[i]['increase'] === -100? 0: stockInfo[i]['increase'].toFixed(2)
-                    };
-                    var tr = changeStockInfoAndColor(stock);
-                    $("#result").append(tr);
+                    for (var i = 0; i < stockInfo.length; ++i) {
+
+                        var stock = {
+                            'name': stockInfo[i]['name'],
+                            'price': stockInfo[i]['currentPrice'] === 0 ? stockInfo[i]['closingPrice'] : stockInfo[i]['currentPrice'],
+                            'increase': stockInfo[i]['increase'] === -100 ? 0 : stockInfo[i]['increase'].toFixed(2)
+                        };
+                        var tr = changeStockInfoAndColor(stock);
+                        $("#result").append(tr);
+                    }
                 }
             },
-            function(data){//query failed
+            function (data) {//query failed
                 showMsg(MSG.NETWORK_ERROR);
             });
-
-        }
 
     });
 }
 
 //query stock information, call done if succeed and call fail if failed
 function queryStockInfo(stockIds, done, fail) {
-    if(!done || !fail ||
+    if (!done || !fail ||
         typeof(done) != 'function' || typeof(fail) != 'function') return;
 
     var jqxhr = $.ajax({
@@ -182,13 +193,16 @@ function queryStockInfo(stockIds, done, fail) {
         .fail(fail);
 }
 
-function checkStockId(id, success){
+/*check whether id is legal or not
+if it's legal success is called, otherwise fail is called
+ */
+function checkStockId(id, success) {
 
-    if(!success || typeof(success) != 'function') return;
+    if (!success || typeof(success) != 'function') return;
 
     var val = '';
 
-    if(id.length != 6) {
+    if (id.length != 6) {
         showMsg(MSG.CODE_SIZE_ERROR);
         return;
     }
@@ -203,10 +217,11 @@ function checkStockId(id, success){
             return;
         }
     }
+
     //query Baidu api check whether id is right or not
-    queryStockInfo(val, function(data){
+    queryStockInfo(val, function (data) {
         //console.log(data);
-        if(data['errMsg'] != 'success' || data['retData']['stockinfo'][0]['name'].length === 0) {
+        if (data['errMsg'] != 'success' || data['retData']['stockinfo'][0]['name'].length === 0) {
             showMsg(MSG.CODE_ERROR);
             return;
         }
@@ -222,12 +237,12 @@ $(document).ready(function () {
     getStockInfo();
 
     //set add event handler
-    $("#AddID").click(function(){
+    $("#AddID").click(function () {
 
         var id = $("#StockID").val();
 
-        checkStockId(id, function(obj) {
-            chrome.storage.sync.set(obj, function(){
+        checkStockId(id, function (obj) {
+            chrome.storage.sync.set(obj, function () {
                 //console.log(obj + 'added!');
                 showMsg(MSG.ADD_INFO);
             });
@@ -239,16 +254,16 @@ $(document).ready(function () {
     });
 
     //set delete event handler
-    $("#DelID").click(function(){
+    $("#DelID").click(function () {
 
         var id = $("#StockID").val();
 
-        checkStockId(id, function(){
+        checkStockId(id, function () {
 
-            chrome.storage.sync.get(null, function(items){
-                for(var key in items){
+            chrome.storage.sync.get(null, function (items) {
+                for (var key in items) {
                     if (id === key) {
-                        chrome.storage.sync.remove(id, function(){
+                        chrome.storage.sync.remove(id, function () {
                             showMsg(MSG.DEL_INFO);
                         });
                         return;
@@ -258,7 +273,6 @@ $(document).ready(function () {
                 showMsg(MSG.CODE_ERROR);
             });
         });
-
         //clear input
         $('#StockID').val('');
     });
